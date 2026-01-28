9541254c3ce25fd6f2100317f09b72bec0f44c2b
Thiago <thiagocascaes@outlook.com,br>
2026-01-28 16:55:30 -0300

    login and invalid login separate

---
+++ cypress/tests/e2e/invalid-login.spec.js
@@ -0,0 +1,14 @@
+const user = require('../users.json')
+
+describe('Unsuccessful Login', () => {
+    beforeEach(() => {
+        cy.visit('/login')
+    })
+
+    it('Login with wrong username or password', () => {
+        cy.get('input[name="username"]').type(user.invalidUser.username)
+        cy.get('input[name="password"]').type(user.invalidUser.password)
+        cy.get('button[type="submit"]').click()
+        cy.get('[role="alert"]').contains('Username or password is invalid').should('be.visible')
+    })
+})
