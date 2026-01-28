9f32333993fa19d07191ee449562299fff9cae54
Thiago <thiagocascaes@outlook.com,br>
2026-01-28 16:40:35 -0300

    Second commit RWA exercise 1

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -11,4 +11,11 @@ describe('Successul Login', () => {
         cy.get('button[type="submit"]').click()
         cy.get('[fill-rule="nonzero"]').should('be.visible')
     })
+
+    it('Login with wrong username or password', () => {
+        cy.get('input[name="username"]').type(user.invalidUser.username)
+        cy.get('input[name="password"]').type(user.invalidUser.password)
+        cy.get('button[type="submit"]').click()
+        cy.get('[role="alert"]').contains('Username or password is invalid').should('be.visible')
+    })
 })
