44dce46c0eda746c83d46c9770e29561bb043cb3
Thiago <thiagocascaes@outlook.com,br>
2026-01-28 18:39:34 -0300

    RWA exercise one complete

---
+++ cypress/tests/e2e/invalid-credentials.spec.js
@@ -0,0 +1,52 @@
+const register = require('../users.json')
+
+describe('Invalid SignUp', () => {
+    beforeEach(() => {
+        cy.visit('/signup')
+    })
+
+    it('Without first name', () => {
+        cy.get('[name="lastName"]').type(register.signUpUser.lastName)
+        cy.get('[name="username"]').type(register.signUpUser.username)
+        cy.get('[name="password"]').type(register.signUpUser.password)
+        cy.get('[name="confirmPassword"]').type(register.signUpUser.password)
+        cy.get('[data-test="signup-first-name"]').contains('First Name is required').should('be.visible')
+    })
+
+    it('Without last name', () => {
+        cy.get('[name="firstName"]').type(register.signUpUser.firstName)
+        cy.get('[name="lastName"]').click()
+        cy.get('[name="username"]').type(register.signUpUser.username)
+        cy.get('[name="password"]').type(register.signUpUser.password)
+        cy.get('[name="confirmPassword"]').type(register.signUpUser.password)
+        cy.get('[data-test="signup-last-name"]').contains('Last Name is required').should('be.visible')
+    })
+
+    it('Without username', () => {
+        cy.get('[name="firstName"]').type(register.signUpUser.firstName)
+        cy.get('[name="lastName"]').type(register.signUpUser.lastName)
+        cy.get('[name="username"]').click()
+        cy.get('[name="password"]').type(register.signUpUser.password)
+        cy.get('[name="confirmPassword"]').type(register.signUpUser.password)
+        cy.get('[data-test="signup-username"]').contains('Username is required').should('be.visible')
+    })
+
+    it('Password at least four characters', () => {
+        cy.get('[name="firstName"]').type(register.signUpUser.firstName)
+        cy.get('[name="lastName"]').type(register.signUpUser.lastName)
+        cy.get('[name="username"]').type(register.signUpUser.username)
+        cy.get('[name="password"]').type(register.invalidCredentials.password)
+        cy.get('[name="confirmPassword"]').type(register.invalidCredentials.password)
+        cy.get('[data-test="signup-password"]').contains('Password must contain at least 4 characters').should('be.visible')
+    })
+
+      it('Password does not match', () => {
+        cy.get('[name="firstName"]').type(register.signUpUser.firstName)
+        cy.get('[name="lastName"]').type(register.signUpUser.lastName)
+        cy.get('[name="username"]').type(register.signUpUser.username)
+        cy.get('[name="password"]').type(register.signUpUser.password)
+        cy.get('[name="confirmPassword"]').type(register.invalidCredentials.password)
+        cy.get('[data-test="signup-confirmPassword"]').contains('Password does not match').should('be.visible')
+    })
+
+})
---
+++ cypress/tests/e2e/signup.spec.js
@@ -0,0 +1,18 @@
+const register = require('../users.json')
+
+describe('Signup', () => {
+    beforeEach(() => {
+        cy.visit('/signup')
+    })
+
+    it('Signup with valid credentials', () => {
+        cy.get('[name="firstName"]').type(register.signUpUser.firstName)
+        cy.get('[name="lastName"]').type(register.signUpUser.lastName)
+        cy.get('[name="username"]').type(register.signUpUser.username)
+        cy.get('[name="password"]').type(register.signUpUser.password)
+        cy.get('[name="confirmPassword"]').type(register.signUpUser.password)
+        cy.get('[type="submit"]').click()
+        cy.get('[href="/signup"]').should('be.visible')
+    })
+
+})
