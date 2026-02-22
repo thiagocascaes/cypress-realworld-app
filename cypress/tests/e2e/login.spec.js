3444076f5c792241d1fad8e2cc3ae58ad77d045d
Thiago <thiagocascaes@outlook.com,br>
2026-02-22 14:01:31 -0300

    Update Login complete with PageObjects

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -6,10 +6,7 @@ const loginPage = new LoginPage()
 describe('Successful Login', () => {
 
     it('Login with valid username and password', () => {
-        loginPage.accessloginPage()
-        cy.get(loginPage.selectorsList.usernameField).type(user.validUser.username)
-        cy.get(loginPage.selectorsList.passwordField).type(user.validUser.password)
-        cy.get(loginPage.selectorsList.submitButton).click()
-        cy.get(loginPage.selectorsList.dashboardIcon).should('be.visible')
+        loginPage.accessLoginPage()
+        loginPage.acessLoginWithUser(user.validUser.username, user.validUser.password)
     })
 })
--- cypress/tests/pages/loginPage.js
+++ cypress/tests/pages/loginPage.js
@@ -1,14 +1,23 @@
 class LoginPage {
-    selectorsList = {
-        usernameField: '[name="username"]',
-        passwordField: '[name="password"]',
-        submitButton: '[type="submit"]',
-        dashboardIcon: '.NavBar-logo'
+    selectorsList() {
+        const selectors = {
+        usernameField: "[name='username']",
+        passwordField: "[name='password']",
+        submitButton: "[type='submit']",
+        dashboardIcon: ".NavBar-logo",
+        }
+        return selectors
+ }
 
+    accessLoginPage() {
+        cy.visit('/login')
     }
 
-    accessloginPage() {
-        cy.visit('/login')
+    acessLoginWithUser(username, password) {
+        cy.get(this.selectorsList().usernameField).type(username)
+        cy.get(this.selectorsList().passwordField).type(password)
+        cy.get(this.selectorsList().submitButton).click()
+        cy.get(this.selectorsList().dashboardIcon).should('be.visible')
     }
 }
 
